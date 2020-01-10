01825bc340e61356e77ff1ea088f4343923ac614
Rye Miller <rye.miller@fruition.net>
2020-01-10 13:20:50 -0700

    Addition of composer file for packagist.

---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade the modules in the
+ * Darkstar suite to newer versions in the future.
+ *
+ * @category  Darkstar
+ * @package   Darkstar_Core
+ * @author    Rye Miller <millerrye17@gmail.com>
+ * @copyright Copyright (c) 2020, Rye Miller (http://ryemiller.com)
+ * @license   MIT License (https://en.wikipedia.org/wiki/MIT_License)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Darkstar_Core',
+    __DIR__
+);
